f6d71be03fe203bed6178cc3525fdf7d50edbf34
Nikita <nikita.h@visualcomposer.com>
2025-02-28 13:34:30 +0200

    Add eslint

--- assets/js/sidebar-for-wpb-admin.js
+++ assets/js/sidebar-for-wpb-admin.js
@@ -1,20 +1,20 @@
 // Simple tooltip behavior using title attribute
-document.addEventListener('DOMContentLoaded', function () {
-	var tooltips = document.querySelectorAll('.sfw-tooltip');
-	tooltips.forEach(function (tooltip) {
-		tooltip.addEventListener('mouseover', function () {
-			var title = tooltip.getAttribute('aria-label');
-			var tooltipDiv = document.createElement('div');
-			tooltipDiv.classList.add('sfw-tooltip-text');
-			tooltipDiv.innerText = title;
-			tooltip.appendChild(tooltipDiv);
-		});
+document.addEventListener('DOMContentLoaded', () => {
+  const tooltips = document.querySelectorAll('.sfw-tooltip')
+  tooltips.forEach(( tooltip ) => {
+    tooltip.addEventListener('mouseover', () => {
+      const title = tooltip.getAttribute('aria-label')
+      const tooltipDiv = document.createElement('div')
+      tooltipDiv.classList.add('sfw-tooltip-text')
+      tooltipDiv.innerText = title
+      tooltip.appendChild(tooltipDiv)
+    })
 
-		tooltip.addEventListener('mouseout', function () {
-			var tooltipDiv = tooltip.querySelector('.sfw-tooltip-text');
-			if (tooltipDiv) {
-				tooltip.removeChild(tooltipDiv);
-			}
-		});
-	});
-});
+    tooltip.addEventListener('mouseout', () => {
+      const tooltipDiv = tooltip.querySelector('.sfw-tooltip-text')
+      if (tooltipDiv) {
+        tooltip.removeChild(tooltipDiv)
+      }
+    })
+  })
+})
--- gulpfile.js
+++ gulpfile.js
@@ -1,78 +1,78 @@
-const gulp = require('gulp');
-const rename = require('gulp-rename');
-const uglify = require('gulp-uglify');
-const cleanCSS = require('gulp-clean-css');
-const mode = require('gulp-mode')();
-const log = require('fancy-log');
+const gulp = require('gulp')
+const rename = require('gulp-rename')
+const uglify = require('gulp-uglify')
+const cleanCSS = require('gulp-clean-css')
+const mode = require('gulp-mode')()
+const log = require('fancy-log')
 
 const folderPaths = {
-	js: './assets/js/',
-	css: './assets/css/',
+  js: './assets/js/',
+  css: './assets/css/'
 }
 const paths = {
-	js: `${folderPaths.js}*.js`,
-	css: `${folderPaths.css}*.css`,
-};
+  js: `${folderPaths.js}*.js`,
+  css: `${folderPaths.css}*.css`
+}
 
 function buildJs() {
-	console.log('Running buildJs...');
+  console.log('Running buildJs...')
 
-	// Exclude already minified files (files with .min.js)
-	let pipeline = gulp.src([paths.js, `!${folderPaths.js}*.min.js`], { allowEmpty: true });
+  // Exclude already minified files (files with .min.js)
+  let pipeline = gulp.src([ paths.js, `!${folderPaths.js}*.min.js` ], { allowEmpty: true })
 
-	if (mode.production()) {
-		console.log('Production mode: Minifying JS...');
-		pipeline = pipeline
-			.pipe(uglify().on('error', log)) // Minify JavaScript in production
-			.pipe(rename({ suffix: '.min' })); // Add .min suffix in production mode
-	} else {
-		console.log('Development mode: No minification, just renaming.');
-		pipeline = pipeline
-			.pipe(rename({ suffix: '.min' })); // Add .min suffix for development without minifying
-	}
+  if (mode.production()) {
+    console.log('Production mode: Minifying JS...')
+    pipeline = pipeline
+      .pipe(uglify().on('error', log)) // Minify JavaScript in production
+      .pipe(rename({ suffix: '.min' })) // Add .min suffix in production mode
+  } else {
+    console.log('Development mode: No minification, just renaming.')
+    pipeline = pipeline
+      .pipe(rename({ suffix: '.min' })) // Add .min suffix for development without minifying
+  }
 
-	return pipeline
-		.pipe(gulp.dest(file => {
-			console.log(`Writing file to: ${file.base}`);
-			return file.base; // Write back to the same folder
-		}))
-		.on('end', () => console.log('buildJs task completed!'));
+  return pipeline
+    .pipe(gulp.dest(file => {
+      console.log(`Writing file to: ${file.base}`)
+      return file.base // Write back to the same folder
+    }))
+    .on('end', () => console.log('buildJs task completed!'))
 }
 
 function buildCss() {
-	console.log('Running buildCss...');
+  console.log('Running buildCss...')
 
-	// Exclude already minified files (files with .min.css)
-	let pipeline = gulp.src([paths.css, `!${folderPaths.css}*.min.css`], { allowEmpty: true });
+  // Exclude already minified files (files with .min.css)
+  let pipeline = gulp.src([ paths.css, `!${folderPaths.css}*.min.css` ], { allowEmpty: true })
 
-	if (mode.production()) {
-		console.log('Production mode: Minifying CSS...');
-		pipeline = pipeline
-			.pipe(cleanCSS({ compatibility: 'ie8' }).on('error', log)) // Minify CSS in production
-			.pipe(rename({ suffix: '.min' })); // Add .min suffix in production mode
-	} else {
-		console.log('Development mode: No minification, just renaming.');
-		pipeline = pipeline
-			.pipe(rename({ suffix: '.min' })); // Add .min suffix for development without minifying
-	}
+  if (mode.production()) {
+    console.log('Production mode: Minifying CSS...')
+    pipeline = pipeline
+      .pipe(cleanCSS({ compatibility: 'ie8' }).on('error', log)) // Minify CSS in production
+      .pipe(rename({ suffix: '.min' })) // Add .min suffix in production mode
+  } else {
+    console.log('Development mode: No minification, just renaming.')
+    pipeline = pipeline
+      .pipe(rename({ suffix: '.min' })) // Add .min suffix for development without minifying
+  }
 
-	return pipeline
-		.pipe(gulp.dest(file => {
-			console.log(`Writing file to: ${file.base}`);
-			return file.base; // Write back to the same folder
-		}))
-		.on('end', () => console.log('buildCss task completed!'));
+  return pipeline
+    .pipe(gulp.dest(file => {
+      console.log(`Writing file to: ${file.base}`)
+      return file.base // Write back to the same folder
+    }))
+    .on('end', () => console.log('buildCss task completed!'))
 }
 
 function watchFiles() {
-	gulp.watch(paths.js, buildJs);
-	gulp.watch(paths.css, buildCss);
+  gulp.watch(paths.js, buildJs)
+  gulp.watch(paths.css, buildCss)
 }
 
 // Define tasks
-const build = gulp.parallel(buildJs, buildCss);
-const watch = gulp.series(build, watchFiles);
+const build = gulp.parallel(buildJs, buildCss)
+const watch = gulp.series(build, watchFiles)
 
 // Export tasks
-exports.watch = watch;
-exports.default = build;
+exports.watch = watch
+exports.default = build
